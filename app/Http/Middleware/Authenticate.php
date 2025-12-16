f930d9a842973ef848e625dbbdc8d5bc91197230
CHIKASHI SHIROZU <102590095+chikashishirozu@users.noreply.github.com>
2025-12-16 17:24:37 +0900

    Refactor redirectTo method in Authenticate middleware
    commit change

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,4 +1,17 @@
-protected function redirectTo(Request $request): ?string
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
+
+class Authenticate extends Middleware
 {
-    return $request->expectsJson() ? null : route('login');
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     */
+    protected function redirectTo(Request $request): ?string
+    {
+        return $request->expectsJson() ? null : route('login');
+    }
 }
